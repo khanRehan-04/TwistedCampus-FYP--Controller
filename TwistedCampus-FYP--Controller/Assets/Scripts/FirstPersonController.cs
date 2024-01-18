6a7f7f62dfc6d515f5bf852ab8df05bf99f03cba
Rehan <109508179+khanRehan-04@users.noreply.github.com>
2024-01-18 14:58:45 +0500

    Added Sprinting & Jumping

--- TwistedCampus-FYP--Controller/Assets/Scripts/FirstPersonController.cs
+++ TwistedCampus-FYP--Controller/Assets/Scripts/FirstPersonController.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FirstPersonController : MonoBehaviour
 {
     public bool CanMove { get; private set; } = true;
+    private bool IsSprinting => canSprint && Input.GetKey(sprintKey);
+    private bool ShouldJump => Input.GetKeyDown(jumpKey) && characterController.isGrounded;
+
+    [Header("Functional Options")]
+    [SerializeField] private bool canSprint = true;
+    [SerializeField] private bool canJump = true;
+
+    [Header("Controls")]
+    [SerializeField] private KeyCode sprintKey = KeyCode.LeftShift;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
 
     [Header("Movement Parameters")]
-    [SerializeField] private float walkSpeed = 3.0f;
-    [SerializeField] private float gravity = 30.0f;
+    [SerializeField] private float walkSpeed = 5.0f;
+    [SerializeField] private float sprintSpeed = 10.0f;
 
     [Header("Mouse Look Parameters")]
     [SerializeField, Range(1, 10)] private float lookSpeedX = 2.0f;
     [SerializeField, Range(1, 10)] private float lookSpeedY = 2.0f;
     [SerializeField, Range(1, 180)] private float upperlookLimit = 80.0f;
     [SerializeField, Range(1, 180)] private float lowerlookLimit = 80.0f;
+
+    [Header("Mouse Look Parameters")]
+    [SerializeField] private float jumpForce = 10.0f;
+    [SerializeField] private float gravity = 24.0f;
 
     private Camera playerCamera;
     private CharacterController characterController;
@@ -25,17 +40,16 @@ public class FirstPersonController : MonoBehaviour
     private float rotationX = 0;
 
     //Animation Components
-    [SerializeField]private Animator characterAnimator;
-    [SerializeField]private string walkAnimationName = "Walking";
-    [SerializeField]private string idleAnimationName = "Idle";
+    //[SerializeField]private Animator characterAnimator;           
+    //[SerializeField]private string walkAnimationName = "Walking";
+    //[SerializeField]private string idleAnimationName = "Idle";
 
-    private bool isWalking = false;
-
-  
-    void Awake()
+    //private bool isWalking = false;                                                          
+                
+    void Awake()                                                                                                                                                                                     
     {
         playerCamera = GetComponentInChildren<Camera>();    
-        characterController = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();                                                                                                               
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;        
     }
@@ -47,15 +61,19 @@ public class FirstPersonController : MonoBehaviour
         {
             HandleMovementInput();
             HandleMouseLook();
+
+            if(canJump)
+                HandleJump();
+
             ApplyFinalMovements();
 
-            UpdateAnimations();
+            //UpdateAnimations();
         }
     }
 
     private void HandleMovementInput()
     {
-        currentInput = new Vector2(walkSpeed * Input.GetAxis("Vertical"), walkSpeed * Input.GetAxis("Horizontal"));
+        currentInput = new Vector2((IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Vertical"), (IsSprinting ? sprintSpeed : walkSpeed) * Input.GetAxis("Horizontal"));
 
         float movDirectionY = movDirection.y;
         movDirection = (transform.TransformDirection(Vector3.forward) * currentInput.x) + (transform.TransformDirection(Vector3.right) * currentInput.y);
@@ -73,6 +91,12 @@ public class FirstPersonController : MonoBehaviour
         transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeedX, 0);
     }
 
+    private void HandleJump()
+    {
+        if (ShouldJump)
+            movDirection.y = jumpForce;
+    }
+
     private void ApplyFinalMovements()
     {
         if (!characterController.isGrounded)
@@ -81,25 +105,25 @@ public class FirstPersonController : MonoBehaviour
         characterController.Move(movDirection * Time.deltaTime);
     }
 
-    private void UpdateAnimations()
-    {
-        if (currentInput.magnitude > 0)
-        {
-            // Player is moving, play walk animation
-            if (!isWalking)
-            {
-                characterAnimator.Play(walkAnimationName);
-                isWalking = true;
-            }
-        }
-        else
-        {
-            // Player is not moving, play idle animation
-            if (isWalking)
-            {
-                characterAnimator.Play(idleAnimationName);
-                isWalking = false;
-            }
-        }
-    }
+    //private void UpdateAnimations()
+    //{
+    //    if (currentInput.magnitude > 0)
+    //    {
+    //        // Player is moving, play walk animation
+    //        if (!isWalking)
+    //        {
+    //            characterAnimator.Play(walkAnimationName);
+    //            isWalking = true;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        // Player is not moving, play idle animation
+    //        if (isWalking)
+    //        {
+    //            characterAnimator.Play(idleAnimationName);
+    //            isWalking = false;
+    //        }
+    //    }
+    //}
 }
